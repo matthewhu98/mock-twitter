f45e57c97478a4e5ed99a986c62bb61099c81f83
Matthew Hu <matt.hu.009@gmail.com>
2020-08-18 14:06:31 -0400

    created database

---
+++ mock-twitter/java/database/database.sql
@@ -0,0 +1,44 @@
+
+DROP TABLE IF EXISTS tweets;
+DROP TABLE IF EXISTS accounts;
+
+
+--CREATE TABLES
+
+CREATE TABLE accounts
+(
+        account_id serial,
+        user_id int NOT NULL,
+        username varchar(28) NOT NULL,
+        accountname varchar(28) NOT NULL,
+        
+        constraint pk_account primary key (account_id),
+        constraint fk_user_account foreign key (user_id) references users(user_id)
+);
+
+CREATE TABLE tweets
+(
+        tweet_id serial,
+        account_id int NOT NULL,
+        tweet_content varchar(280) NOT NULL,
+        date_of_tweet date NOT NULL,
+        
+        constraint pk_tweet primary key (tweet_id),
+        constraint fk_accounts_tweets foreign key (account_id) references accounts(account_id)
+);
+
+
+--INSERT STATEMENTS
+
+START TRANSACTION;
+INSERT INTO accounts (account_id, user_id, username, accountname)
+VALUES (1, 1, 'testuser', 'testuser'), (2, 2, 'testadmin', 'testadmin');
+COMMIT
+
+START TRANSACTION;
+INSERT INTO tweets (tweet_id, account_id, tweet_content, date_of_tweet)
+VALUES (1, 1, 'test tweet from user', CURRENT_DATE);
+
+SELECT *
+from tweets;
+COMMIT;
